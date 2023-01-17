cad4c0b0406ef1da213b560a33ccbd0ee74f38a8
shakedal <salmog510@gmail.com>
2023-01-17 23:59:15 +0200

    improve categorical experiments add simulate drift by changing order

--- DriftSimulationRun.py
+++ DriftSimulationRun.py
@@ -38,9 +38,13 @@ severity = {
     "one_std_down": 2,
     "two_std_up": 3,
     "two_std_down": 3,
-    "0.1": 1,
-    "0.2": 2,
-    "0.3": 3
+    "0.3": 1,
+    "0.5": 2,
+    "0.7": 3,
+    "0.9": 4
+    # "0.1": 1,
+    # "0.2": 2,
+    # "0.3": 3
     # "uniform dist": 2,
     # "original dist": 2,
     # "filtered dist": 1,
@@ -109,21 +113,21 @@ def change_data_binary2(feature, all_data, filtered_data, indexes_filtered_data)
     values = all_data[feature].unique()
     feature_changes = []
     feature_changes_names = []
-    changes = [0.1, 0.2, 0.3]
+    changes = [0.3, 0.5, 0.7, 0.9]
     original = filtered_data.copy()
 
     for val in values:
         to_change = original.copy()
         indexes = to_change[feature] != val
-        n = indexes.count()
-        indexes = indexes.index.values
+        n = indexes.values.sum()
+        indexes = to_change[indexes].index.values
         if n < 1:
             continue
         for p in changes:
             to_change = original.copy()
             indexes_to_change = random.choices(indexes, k=int(n*p))
             to_change.loc[indexes_to_change, feature] = val
-            feature_changes.append(to_change)
+            feature_changes.append(to_change[feature])
             feature_changes_names.append(f"{p}")
 
     # feature_changes = [uniform_dist, orig_dist, filtered_dist, softmax_orig_dist, softmax_filtered_dist]
@@ -154,43 +158,71 @@ def manipulate_feature(feature, feature_type, dataset):
         to_save = not_changed_data.append(to_save, ignore_index=True)
         yield to_save, change_name
 
+def simulate_drift(feature, feature_type, dataset):
+    # order indexes by feature value
+    sorted_data = dataset.data.sort_values(by=feature)
+
+    # divide to before & after
+    n_samples = sorted_data.shape[0]
+    n_before = dataset.before_size
+    n_after = n_samples - n_before
+    indexes_before_drift = list(range(n_before))
+    indexes_after_drift = list(range(n_before, n_samples))
+
+    # sample before
+    indexes_before = random.sample(indexes_before_drift, k=int(0.8*n_before))
+    indexes_before += random.sample(indexes_after_drift, k=int(0.2*n_before))
+    assert len(indexes_before) == len(set(indexes_before))
+
+    # sample after + test
+    indexes_after = list(filter(lambda x: x not in indexes_before, range(n_samples)))
+    assert len(indexes_before) + len(indexes_after) == n_samples
+
+    # combine to one df
+    all_indexes = indexes_before + indexes_after
+    drift_data = sorted_data.iloc[all_indexes]
+    drift_data = drift_data.reset_index(drop=True)
+
+    return drift_data
+
 def is_feature_in_tree(tree_rep, feature_num):
     for node, d in tree_rep.items():
         if d["feature"] == feature_num:
             return True
     return False
 
+all_sizes = [
+    (0.7, 0.1, 0.2),
+    (0.7, 0.07, 0.2),
+    (0.7, 0.05, 0.2),
+    (0.7, 0.02, 0.2),
+    (0.7, 0.01, 0.2),
+    (0.7, 0.005, 0.2)
+]
+
+# all_sizes = [
+#     (0.6, 0.2, 0.2),
+#     (0.6, 0.1, 0.2),
+#     (0.6, 0.07, 0.2),
+#     (0.6, 0.05, 0.2),
+#     (0.6, 0.02, 0.2),
+#     (0.6, 0.01, 0.2),
+#     (0.6, 0.005, 0.2)
+# ]
+
 if __name__ == '__main__':
     all_results = []
     time_stamp = datetime.now()
     date_time = time_stamp.strftime("%d-%m-%Y__%H-%M-%S")
 
-    all_sizes = [
-        (0.7, 0.1, 0.2),
-        (0.7, 0.07, 0.2),
-        (0.7, 0.05, 0.2),
-        (0.7, 0.02, 0.2),
-        (0.7, 0.01, 0.2),
-        (0.7, 0.005, 0.2)
-    ]
-
-    # all_sizes = [
-    #     (0.6, 0.2, 0.2),
-    #     (0.6, 0.1, 0.2),
-    #     (0.6, 0.07, 0.2),
-    #     (0.6, 0.05, 0.2),
-    #     (0.6, 0.02, 0.2),
-    #     (0.6, 0.01, 0.2),
-    #     (0.6, 0.005, 0.2)
-    # ]
-
     all_datasets = pd.read_csv('data/all_datasets.csv', index_col=0)
 
-    categorical_datasets = ["analcatdata_boxing1", "braziltourism", "meta", "newton_hema", "socmob", "vote", "newton_hema", "visualizing_livestock"]
+    # categorical_datasets = ["visualizing_livestock"]
+    # categorical_datasets = ["analcatdata_boxing1", "braziltourism", "meta", "newton_hema", "socmob", "vote", "newton_hema", "visualizing_livestock"]
 
     for index, row in all_datasets.iterrows():
-        if row["name"] not in categorical_datasets:
-            continue
+        # if row["name"] not in categorical_datasets:
+        #     continue
         # if index > 3:  # use for testing
         #     break
 
@@ -277,3 +309,66 @@ if __name__ == '__main__':
     write_to_excel(all_results, f"result_run_{date_time}")
 
     print("DONE")
+
+# if __name__ == '__main__':
+#     all_results = []
+#     time_stamp = datetime.now()
+#     date_time = time_stamp.strftime("%d-%m-%Y__%H-%M-%S")
+#
+#     all_datasets = pd.read_csv('data/all_datasets.csv', index_col=0)
+#     for index, row in all_datasets.iterrows():
+#         # if index > 3:  # use for testing
+#         #     break
+#
+#         print(f'------------------DATASET: {row["name"]}------------------')
+#         data_size = row["dataset size"]
+#
+#         dataset = DataSet(row["path"].replace("\\", "/"), "diagnosis_check", None, None, (0.7, 0.1, 0.2),
+#                           name=row["name"], to_shuffle=False)
+#
+#         # manipulate data & run experiment
+#         for i in range(len(dataset.features)):
+#             feature = dataset.features[i]
+#             feature_type = dataset.feature_types[i]
+#             manipulated_data = simulate_drift(feature, feature_type, dataset)
+#             dataset = DataSet(manipulated_data.copy(), "diagnosis_check", None, None, (0.7, 0.1, 0.2),
+#                               name=row["name"], to_shuffle=False)
+#
+#             # build tree
+#             concept_size = dataset.before_size
+#             target = dataset.target
+#             feature_types = dataset.feature_types
+#             train = dataset.data.iloc[0:int(0.9 * concept_size)].copy()
+#             validation = dataset.data.iloc[int(0.9 * concept_size):concept_size].copy()
+#             model = build_model(train, dataset.features, dataset.target, val_data=validation)
+#             tree_rep = map_tree(model)
+#             model = prune_tree(model, tree_rep)
+#             print("TREE:")
+#             print_tree_rules(model, dataset.features)
+#             tree_rep = map_tree(model)
+#
+#             # pass if feature not in tree
+#             if not is_feature_in_tree(tree_rep, i):
+#                 continue
+#
+#             # experiments
+#             for sizes in all_sizes:
+#                 # check if there is enough data for experiment
+#                 if sizes[1] * data_size < 1:
+#                     continue
+#
+#                 dataset1 = DataSet(manipulated_data.copy(), "diagnosis_check", None, None, sizes,
+#                                   name=row["name"], to_shuffle=False)
+#                 with HiddenPrints():
+#                     result = run_single_tree_experiment(dataset1, model=copy.deepcopy(model),
+#                                                         check_diagnosis=False, faulty_nodes=[i])
+#
+#                 result["size"] = sizes[1]
+#                 result["dataset"] = dataset.name
+#                 result["feature type"] = feature_type
+#                 result["number of faulty nodes"] = 1
+#                 all_results.append(result)
+#
+#     write_to_excel(all_results, f"result_run_{date_time}")
+#
+#     print("DONE")

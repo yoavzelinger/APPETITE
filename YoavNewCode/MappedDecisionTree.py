34a954bf0f613ccccbbc2e20474e3724da511f7a
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2024-11-26 18:44:56 +0200

    Finishing prototype of MappedDecisionTree

--- YoavNewCode/MappedDecisionTree.py
+++ YoavNewCode/MappedDecisionTree.py
@@ -1,4 +1,5 @@
 from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 
 class MappedDecisionTree:
     class DecisionTreeNode:
@@ -9,33 +10,17 @@ class MappedDecisionTree:
                      right_child: 'MappedDecisionTree.DecisionTreeNode' | None = None,
                      feature: str | None = None,
                      threshold: int | None = None, # TODO - Verify Type
-                     label: str | None = None
+                     class_name: str | None = None
         ):
             self.index = index
             self.parent = parent
             self.update_children(left_child, right_child)
-            if self.is_terminal():
-                assert label is not None
-            self.label = label
             self.feature = feature
             self.threshold = threshold
+            self.class_name = class_name
             self.condition = []
-            self._calculate_condition()
-            self.label = label
             self.depth = 0 if parent is None else parent.depth + 1
-
-        def _calculate_condition(self):
-            if self.parent in None:
-                return
-            self.condition += self.parent.condition
-            current_condition = {
-                "feature": self.parent.feature,
-                "threshold": self.parent.threshold,
-                "sign": "<=" if self.is_left_child() else ">"
-            }
-            self.condition += [current_condition]
-
-            
+            self.update_condition()
 
         def update_children(self, 
                             left_child: 'MappedDecisionTree.DecisionTreeNode', 
@@ -45,6 +30,17 @@ class MappedDecisionTree:
             assert (left_child is None) == (right_child is None)
             self.left_child = left_child
             self.right_child = right_child
+
+        def update_condition(self):
+            if self.parent in None:
+                return
+            self.condition += self.parent.condition
+            current_condition = {
+                "feature": self.parent.feature,
+                "threshold": self.parent.threshold,
+                "sign": "<=" if self.is_left_child() else ">"
+            }
+            self.condition += [current_condition]
 
         def is_terminal(self) -> bool:
             return self.left_child is None
@@ -68,57 +64,39 @@ class MappedDecisionTree:
         self.n_classes = sklearn_tree.n_classes_
 
         self.tree_dict = self.map_tree(sklearn_tree)
+        self.root = self.tree_dict[0]
 
     def map_tree(self, 
                  sklearn_tree: DecisionTreeClassifier
     ) -> dict[int, 'MappedDecisionTree.DecisionTreeNode']:
-        threshold = sklearn_tree.tree_.threshold
-        feature = sklearn_tree.tree_.feature
-        children_left = sklearn_tree.tree_.children_left
-        children_right = sklearn_tree.tree_.children_right
-        value = sklearn_tree.tree_.value
-        class_names = sklearn_tree.classes_
+        sk_features = sklearn_tree.tree_.feature
+        sk_thresholds = sklearn_tree.tree_.threshold
+        sk_children_left = sklearn_tree.tree_.children_left
+        sk_children_right = sklearn_tree.tree_.children_right
+        sk_values = sklearn_tree.tree_.value
+        sk_class_names = sklearn_tree.classes_
         
         tree_representation = {}
-        nodes_to_check = [0]
+        nodes_to_check = [self.DecisionTreeNode(index=0)]
+
         while len(nodes_to_check):
-            # TODO: Continue
-            node = nodes_to_check.pop(0)
+            current_node = nodes_to_check.pop(0)
+            current_index = current_node.index
+            tree_representation[current_index] = current_node
+            left_child_index = sk_children_left[current_index]
 
-            left_child_index = children_left[node]
-            tree_representation[node]["left"] = left_child
-            if left_child != -1:
-                tree_representation[left_child] = {"parent": node,
-                                                   "type": "left"}
-                nodes_to_check.append(left_child)
-            right_child = self.children_right[node]
-            tree_representation[node]["right"] = right_child
-            if right_child != -1:
-                tree_representation[right_child] = {"parent": node,
-                                                    "type": "right"}
-                nodes_to_check.append(right_child)
+            if left_child_index == -1:  # Leaf
+                current_node_value = sk_values[current_index]
+                class_name = np.argmax(current_node_value)
+                class_name = sk_class_names[class_name]
+                current_node.class_name = class_name
+                continue
 
-            tree_representation[node]["feature"] = self.feature[node]
-            tree_representation[node]["threshold"] = self.threshold[node]
-
-            if node != 0:
-                parent = tree_representation[node]["parent"]
-                tree_representation[node]["depth"] = tree_representation[parent]["depth"] + 1
-                parent_cond = tree_representation[parent]["condition"]
-                sign = "<=" if tree_representation[node]["type"] == "left" else ">"
-                cond = {
-                    "feature": self.feature[parent],
-                    "sign": sign,
-                    "thresh": self.threshold[parent]
-                }
-                tree_representation[node]["condition"] = parent_cond + [cond]
-            else:  # root
-                tree_representation[node]["condition"] = []
-
-            if left_child == -1:  # leaf
-                value = self.value[node]
-                class_name = np.argmax(value)
-                class_name = class_names[class_name]
-                tree_representation[node]["class"] = class_name
+            current_node.feature, current_node.threshold = sk_features[current_index], sk_thresholds[current_index]
+            right_child_index = sk_children_right[current_node]
+            for child_index in (left_child_index, right_child_index):
+                child_node = self.DecisionTreeNode(index=child_index, parent=current_node)
+                nodes_to_check.append(child_node)
+            current_node.update_children(*(nodes_to_check[-2: ]))
 
         return tree_representation

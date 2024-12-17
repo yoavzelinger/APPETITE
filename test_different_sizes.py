efafd119c51eccebf482aa3338b20af5658b1136
Yoav Zelinger <yoavzelinger11@gmail.com>
2024-12-17 09:01:58 +0200

    Update test_different_sizes.py

--- test_different_sizes.py
+++ test_different_sizes.py
@@ -32,43 +32,44 @@ for diagnoser_name in DEFAULT_TESTING_DIAGNOSER:
     total_results_columns.append(diagnoser_name + FIX_ACCURACY_NAME_SUFFIX)
     total_results_columns.append(diagnoser_name + FIX_ACCURACY_INCREASE_NAME_SUFFIX)
 
-# Create DataFrame for the aggregated results
-total_results = DataFrame(columns=[SIZES_COLUMN_NAME, COUNT_COLUMN_NAME] + total_results_columns)
+if __name__ == "__main__":
+    # Create DataFrame for the aggregated results
+    total_results = DataFrame(columns=[SIZES_COLUMN_NAME, COUNT_COLUMN_NAME] + total_results_columns)
 
-TRAIN_AFTER_PERCENTAGE_SIZE = int((1 - TEST_PROPORTION) * 100)
+    TRAIN_AFTER_PERCENTAGE_SIZE = int((1 - TEST_PROPORTION) * 100)
 
-for train_size in range(5, TRAIN_AFTER_PERCENTAGE_SIZE, 5):
-    with open(f"{DATA_DIRECTORY}/{DATASET_DESCRIPTION_FILE}", "r") as descriptions_file:
-        train_proportion, after_proportion = train_size / 100, (TRAIN_AFTER_PERCENTAGE_SIZE - train_size) / 100
-        print("XXXXXXXXXXXXXXXXXXXXXXXX", train_proportion, after_proportion, TEST_PROPORTION, "XXXXXXXXXXXXXXXXXXXXXXXX")
+    for train_size in range(5, TRAIN_AFTER_PERCENTAGE_SIZE, 5):
+        with open(f"{DATA_DIRECTORY}/{DATASET_DESCRIPTION_FILE}", "r") as descriptions_file:
+            train_proportion, after_proportion = train_size / 100, (TRAIN_AFTER_PERCENTAGE_SIZE - train_size) / 100
+            print("XXXXXXXXXXXXXXXXXXXXXXXX", train_proportion, after_proportion, TEST_PROPORTION, "XXXXXXXXXXXXXXXXXXXXXXXX")
 
-        current_total_row_dict = {SIZES_COLUMN_NAME: f"{train_proportion}-{after_proportion}-{TEST_PROPORTION}"
-                                  , COUNT_COLUMN_NAME: 0}
-        current_total_row_dict.update({total_results_column: 0 for total_results_column in total_results_columns})
-        
-        proportion_tuple = (train_proportion, after_proportion, TEST_PROPORTION)
-
-        descriptions_reader = DictReader(descriptions_file)
-        for dataset_description in descriptions_reader:
-            dataset_name = dataset_description["name"]
+            current_total_row_dict = {SIZES_COLUMN_NAME: f"{train_proportion}-{after_proportion}-{TEST_PROPORTION}"
+                                    , COUNT_COLUMN_NAME: 0}
+            current_total_row_dict.update({total_results_column: 0 for total_results_column in total_results_columns})
             
-            try:
-                for test_result in run_test(DATASETS_FULL_PATH, dataset_name + ".csv", proportions_tuple=proportion_tuple):
-                    current_total_row_dict[COUNT_COLUMN_NAME] = current_total_row_dict[COUNT_COLUMN_NAME] + 1
-                    for key, value in test_result.items():
-                        if key in total_results_columns:
-                            current_total_row_dict[key] += value
-            except Exception as e:
-                if isinstance(e, KeyError):
-                    raise(e)
-                continue
+            proportion_tuple = (train_proportion, after_proportion, TEST_PROPORTION)
+
+            descriptions_reader = DictReader(descriptions_file)
+            for dataset_description in descriptions_reader:
+                dataset_name = dataset_description["name"]
+                
+                try:
+                    for test_result in run_test(DATASETS_FULL_PATH, dataset_name + ".csv", proportions_tuple=proportion_tuple):
+                        current_total_row_dict[COUNT_COLUMN_NAME] = current_total_row_dict[COUNT_COLUMN_NAME] + 1
+                        for key, value in test_result.items():
+                            if key in total_results_columns:
+                                current_total_row_dict[key] += value
+                except Exception as e:
+                    if isinstance(e, KeyError):
+                        raise(e)
+                    continue
+            
+            current_total_row_dict.update({total_results_column: current_total_row_dict[total_results_column] / current_total_row_dict[COUNT_COLUMN_NAME] for total_results_column in total_results_columns})
+            total_results = total_results._append(current_total_row_dict, ignore_index=True)
+
+    if not os_path_exists(RESULTS_FULL_PATH):
+        os_mkdir(RESULTS_FULL_PATH)
         
-        current_total_row_dict.update({total_results_column: current_total_row_dict[total_results_column] / current_total_row_dict[COUNT_COLUMN_NAME] for total_results_column in total_results_columns})
-        total_results = total_results._append(current_total_row_dict, ignore_index=True)
+    total_results.to_csv(f"{RESULTS_FULL_PATH}total_results.csv", index=False)
 
-if not os_path_exists(RESULTS_FULL_PATH):
-    os_mkdir(RESULTS_FULL_PATH)
-    
-total_results.to_csv(f"{RESULTS_FULL_PATH}total_results.csv", index=False)
-
-print(f"Total time: {time() - start_time}")
+    print(f"Total time: {time() - start_time}")

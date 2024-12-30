f06d3f07168c33bdb11d8623df2ea0f1a8d33065
Yoav Zelinger <yoavzelinger11@gmail.com>
2024-12-31 01:36:49 +0200

    BARINEL finish phase 0

--- APPETITE/Diagnosers/BARINEL.py
+++ APPETITE/Diagnosers/BARINEL.py
@@ -1,10 +1,12 @@
+from numpy import ndarray, array
 from pandas import DataFrame, Series
 
-# from barinel_utils.methods_for_diagnosis import diagnosis_0 as _barinel_diagnosis
+from APPETITE.Constants import GRADIENT_STEP
+from APPETITE.DecisionTreeTools.MappedDecisionTree import MappedDecisionTree
 from .barinel_utils import *
 from .SFLDT import SFLDT
 
-def get_barinel_diagnoses(spectra: DataFrame,
+def get_barinel_diagnoses(spectra: ndarray,
                           error_vector: Series) -> list[list[int]]:
     """
     Perform the Barinel diagnosis algorithm on the given spectrum.
@@ -15,32 +17,38 @@ def get_barinel_diagnoses(spectra: DataFrame,
     Returns:
     list[list[int]]: All the diagnoses.
     """
-    extended_spectra = list(map(lambda spectra_vector_pair: spectra_vector_pair[0] + [spectra_vector_pair[1]], zip(spectra.values.tolist(), error_vector.tolist())))
-    diagnoses, _ = _barinel_diagnosis(extended_spectra, [])
+    spectrum = list(map(lambda spectra_vector_pair: spectra_vector_pair[0] + [spectra_vector_pair[1]], zip(spectra.T.tolist(), error_vector.tolist())))
+    diagnoses, _ = _barinel_diagnosis(spectrum, [])
+    diagnoses = _rank_diagnoses(spectrum, diagnoses, GRADIENT_STEP)
+    diagnoses = [(diagnosis[0], diagnosis[1]) for diagnosis in diagnoses]
     return diagnoses
 
 class BARINEL(SFLDT):
-    def __init__(mapped_tree, X, y):
+    def __init__(self, 
+                 mapped_tree: MappedDecisionTree,
+                 X: DataFrame,
+                 y: Series
+    ):
         super().__init__(mapped_tree, X, y)
 
-    def get_diagnosis(self,
-                      retrieve_spectra_indices: bool = False
-    ) -> list[list[int]]:
+    def get_diagnoses(self,
+                      retrieve_spectra_indices: bool = False,
+                      retrieve_ranks: bool = False
+     ) -> list[int] | list[tuple[int, float]]:
         """
         Get the diagnosis of the nodes.
         The diagnosis consists the nodes ordered.
 
         Parameters:
         retrieve_spectra_indices (bool): Whether to return the spectra indices or the node indices.
+        retrieve_ranks (bool): Whether to return the diagnosis ranks.
 
         Returns:
-        list[list[int]]: The diagnoses.
+        list[int] | list[tuple[int, float]]: The diagnosis. If retrieve_ranks is True, the diagnosis will be a list of tuples,
+          where the first element is the index and the second is the similarity rank.
         """
-        diagnoses = get_barinel_diagnoses(self.spectra, self.error_vector)
-        if retrieve_spectra_indices: 
-            return diagnoses
-        convert_index_func = self.mapped_tree.convert_spectra_index_to_node_index
-        for diagnosis in diagnoses:
-            for diagnosis_node_index, spectra_index in enumerate(diagnosis):
-                diagnosis[diagnosis_node_index] = convert_index_func(spectra_index)
-        return diagnoses
+        if self.diagnoses is None:
+            self.diagnoses = get_barinel_diagnoses(self.spectra, self.error_vector)
+            self.sort_diagnoses()
+        return super().get_diagnoses(retrieve_ranks, retrieve_spectra_indices)
+        

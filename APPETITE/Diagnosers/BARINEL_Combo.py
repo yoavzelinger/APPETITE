6b5100087dbbd4284ee0c6d7aa348ac5c04b1c27
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-01-18 18:37:25 +0200

    Fixing BARINEL_Combo

--- APPETITE/Diagnosers/BARINEL_Combo.py
+++ APPETITE/Diagnosers/BARINEL_Combo.py
@@ -1,9 +1,7 @@
-from pandas import DataFrame, Series
-
-from APPETITE.DecisionTreeTools.MappedDecisionTree import MappedDecisionTree
+from .ADiagnoser import *
 from .BARINEL import BARINEL
 
-class BARINEL_Combo(BARINEL):
+class BARINEL_Combo(ADiagnoser):
     """
     The diagnoser that combines the STAT and BARINEL diagnosers.
     """
@@ -21,6 +19,7 @@ class BARINEL_Combo(BARINEL):
         y (Series): The target column.
         """
         super().__init__(mapped_tree, X, y)
+        self.barinel = BARINEL(mapped_tree, X, y)
     
     def get_diagnoses(self,
                       retrieve_ranks: bool = False,
@@ -35,19 +34,18 @@ class BARINEL_Combo(BARINEL):
         list[int] | list[tuple[int, float]]: The diagnoses. If retrieve_ranks is True, the diagnoses will be a list of tuples with the node index and the rank.
         """
         if self.diagnoses is None:
-            if self.barinel_diagnoses is None:
-                self.barinel_diagnoses = super().get_diagnoses(retrieve_ranks=True)
+            barinel_diagnoses = self.barinel.get_diagnoses(retrieve_ranks=True, retrieve_spectra_indices=retrieve_spectra_indices)
             features_cumulative_diagnoses = {feature: (0, 0) for feature in self.mapped_tree.tree_features_set}
-            for diagnosis, rank in self.barinel_diagnoses:
-                for diagnosis_faulty_feature in map(lambda node_index: self.mapped_tree.get_node(node_index).get_feature_extended(), diagnosis):
+            for diagnosis, rank in barinel_diagnoses:
+                for diagnosis_faulty_feature in map(lambda node_index: self.mapped_tree.get_node(node_index, use_spectra_index=retrieve_spectra_indices).get_feature_extended(), diagnosis):
                     features_cumulative_diagnoses[diagnosis_faulty_feature] = (features_cumulative_diagnoses[diagnosis_faulty_feature][0] + rank, features_cumulative_diagnoses[diagnosis_faulty_feature][1] + 1)
 
             self.diagnoses = []
-            for diagnosis, _ in self.barinel_diagnoses:
+            for diagnosis, _ in barinel_diagnoses:
                 features_ranks_sum, features_diagnoses_count = 0, 0
-                for diagnosis_faulty_feature in map(lambda node_index: self.mapped_tree.get_node(node_index).get_feature_extended(), diagnosis):
+                for diagnosis_faulty_feature in map(lambda node_index: self.mapped_tree.get_node(node_index, use_spectra_index=retrieve_spectra_indices).get_feature_extended(), diagnosis):
                     features_ranks_sum += features_cumulative_diagnoses[diagnosis_faulty_feature][0]
                     features_diagnoses_count += features_cumulative_diagnoses[diagnosis_faulty_feature][1]
                 self.diagnoses.append((diagnosis, features_ranks_sum / features_diagnoses_count))
             self.sort_diagnoses()
-        return super().get_diagnoses(retrieve_ranks, retrieve_spectra_indices)
+        return super().get_diagnoses(retrieve_ranks)

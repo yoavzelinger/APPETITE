010df501866cee6da020a57a7686ab60aaa53578
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-01-25 13:43:30 +0200

    adjusting STAT_BARINEL for easy adjustment of STAT type

--- APPETITE/Diagnosers/STAT_BARINEL.py
+++ APPETITE/Diagnosers/STAT_BARINEL.py
@@ -2,6 +2,8 @@ from .ADiagnoser import *
 from .STAT import STAT
 from .BARINEL import BARINEL
 
+from APPETITE.Constants import BARINEL_STAT_TYPE
+
 class STAT_BARINEL(ADiagnoser):
     """
     The diagnoser that combines the STAT and BARINEL diagnosers.
@@ -9,7 +11,8 @@ class STAT_BARINEL(ADiagnoser):
     def __init__(self, 
                  mapped_tree: MappedDecisionTree,
                  X: DataFrame,
-                 y: Series
+                 y: Series,
+                 stat_type: str = BARINEL_STAT_TYPE
     ):
         """
         Initialize the diagnoser.
@@ -22,29 +25,21 @@ class STAT_BARINEL(ADiagnoser):
         super().__init__(mapped_tree, X, y)
         self.stat = STAT(mapped_tree, X, y)
         self.barinel = BARINEL(mapped_tree, X, y)
+        self.get_stat_violation = self.get_stat_type(stat_type)
     
-    def get_stat_diagnoses(self,
-                           diagnosis_type: str = 'difference',   # | 'before' | 'after'
-     ) -> list[tuple[int, float]]:
-        """
-        Get the STAT diagnoses.
-
-        Parameters:
-        diagnosis_type (str): The type of diagnosis. It can be 'difference', 'before', or 'after'.
-
-        Returns:
-        list[tuple[int, float]]: The STAT diagnoses with their ranks.
-        """
-        assert diagnosis_type in ['difference', 'before', 'after']
-        if diagnosis_type == 'difference':
-            return [(node_index, self.stat.get_node_violation_difference(node_index)) for node_index in self.mapped_tree.get_node_indices()]
-        elif diagnosis_type == 'before':
-            return [(node_index, self.stat.get_before_violation(node_index)) for node_index in self.mapped_tree.get_node_indices()]
-        return [(node_index, self.stat.get_after_violation(node_index)) for node_index in self.mapped_tree.get_node_indices()]
+    def get_stat_type(self,
+                      type: str
+    ):
+        if type == "BEFORE":
+            return self.stat.get_before_violation
+        if type == "AFTER":
+            return self.stat.get_after_violation
+        if type == "DIFFERENCE":
+            return self.stat.get_node_violation_difference
 
     def get_diagnoses(self,
                       retrieve_ranks: bool = False,
-                      retrieve_spectra_indices: bool = False,
+                      retrieve_spectra_indices: bool = False
      ) -> list[int] | list[tuple[int, float]]:
         """
         Get the diagnoses.
@@ -58,11 +53,11 @@ class STAT_BARINEL(ADiagnoser):
         if self.diagnoses is None:
             self.diagnoses = self.barinel.get_diagnoses(retrieve_ranks=True, retrieve_spectra_indices=retrieve_spectra_indices)
             for diagnosis_index, (diagnosis, barinel_rank) in enumerate(self.diagnoses):
-                stat_rank = 1
+                combined_rank = barinel_rank
                 for node_index in diagnosis:
                     if retrieve_spectra_indices:
                         node_index = self.mapped_tree.convert_spectra_index_to_node_index(node_index)
-                    stat_rank *= self.stat.get_after_violation(node_index)
-                self.diagnoses[diagnosis_index] = (diagnosis, barinel_rank * stat_rank)
+                    combined_rank *= self.get_stat_violation(node_index)
+                self.diagnoses[diagnosis_index] = (diagnosis, combined_rank)
             self.sort_diagnoses()
         return super().get_diagnoses(retrieve_ranks)

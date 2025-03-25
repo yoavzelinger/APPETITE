c5d0e6d56db4b06d98ced5356781de116b98cd2a
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-03-25 11:14:19 +0200

    add documentation

--- APPETITE/Diagnosers/FuzzySFLDT.py
+++ APPETITE/Diagnosers/FuzzySFLDT.py
@@ -3,6 +3,16 @@ from numpy import ones, newaxis
 from .SFLDT import SFLDT
 
 class FuzzySFLDT(SFLDT):
+    """
+    The FuzzySFLDT diagnoser.
+    
+    This diagnoser is an extension of the SFLDT diagnoser, where the participation of the nodes is fuzzy.
+    """
+    
     def update_fuzzy_participation(self,
      ) -> None:
+        """
+        Update the fuzzy participation of the nodes.
+        The participation is calculated as the depth of the component normalized by the depth of the path.
+        """
         self.spectra = self.spectra * (self.components_depths_vector + ones(self.node_count))[:, newaxis] / self.paths_depths_vector[newaxis, :]

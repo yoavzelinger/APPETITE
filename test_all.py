f9d46468f215af1356e817e6f0f51be2e0ba426e
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-04-15 11:02:22 +0300

    rename column

--- test_all.py
+++ test_all.py
@@ -43,7 +43,7 @@ if SPECIFIC_DATASETS:
     print(f"Running tests with prefixes: {SPECIFIC_DATASETS}")
     SPECIFIC_DATASETS = list(map(lambda prefix: prefix.lower(), SPECIFIC_DATASETS))
 
-raw_results_columns = ["after size", "drift amount", "drift description", "drifted features types", "tree size", "after accuracy decrease", "after retrain accuracy", "after retrain accuracy increase", "before after retrain accuracy", "before after retrain accuracy increase"]
+raw_results_columns = ["after size", "drift size", "drift description", "drifted features types", "tree size", "after accuracy decrease", "after retrain accuracy", "after retrain accuracy increase", "before after retrain accuracy", "before after retrain accuracy increase"]
 aggregated_groupby_columns = ["name", "tree size", "drifts count"]
 
 aggregated_summarizes_columns = ["average after accuracy decrease", "average after retrain accuracy", "average after retrain accuracy increase", "average before after retrain accuracy", "average before after retrain accuracy increase"]

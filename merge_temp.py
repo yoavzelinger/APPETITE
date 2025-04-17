ec48e5707d5d54f3d374d6c43f3062605251b491
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-04-17 22:18:24 +0300

    update the merge temp to support

--- merge_temp.py
+++ merge_temp.py
@@ -8,7 +8,6 @@ from Tester import *
 
 parser = ArgumentParser(description="Run all tests")
 parser.add_argument("-o", "--output", type=str, help="Output file name prefix, default is the result_TIMESTAMP", default=f"{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}")
-# add flag -d to deterime if the user wants to clear the temporary files
 parser.add_argument("-c", "--clear", action="store_true", help="Clear the temporary files after merging, default is false", default=False)
 args = parser.parse_args()
 
@@ -18,23 +17,30 @@ searching_file_prefix = tester_constants.RESULTS_FILE_NAME_PREFIX
 
 output_file_name = f"{tester_constants.RESULTS_FILE_NAME_PREFIX}_{args.output}.csv"
 
-# merge all files in the directory to a single file
-output_df = DataFrame()
+primary_key_columns = ["drift description", "after size"]
+common_columns = ["drift size", "drifted features types", "tree size", "after accuracy decrease", "after retrain accuracy", "after retrain accuracy increase", "before after retrain accuracy", "before after retrain accuracy increase"]
+output_df = DataFrame(columns=primary_key_columns + common_columns).set_index(primary_key_columns)
 
 for file_name in listdir(searching_directory):
     if file_name.startswith(searching_file_prefix):
         result_file_path = os_path.join(searching_directory, file_name)
         with open(result_file_path, "r") as file:
-            result_file_reader = DictReader(file)
-            result_df = DataFrame(result_file_reader)
-            output_df = DataFrame._append(output_df, result_df, ignore_index=True)
-        if args.clear:  # remove the file after merging
+            result_df = DataFrame(DictReader(file))
+            result_df = result_df.set_index(primary_key_columns)
+            output_df = output_df.combine_first(result_df)
+        if args.clear:
             print(f"Removing {result_file_path}")
             os_remove(result_file_path)
+
+# reorder columns
+ordered_columns = common_columns
+ordered_columns += [column for column in output_df.columns if column not in primary_key_columns and column not in common_columns and "fuzzy" not in column] # regulars
+ordered_columns += [column for column in output_df.columns if column not in ordered_columns] # fuzzy
+output_df = output_df[ordered_columns]
 
 if output_df.empty:
     print("temp folder do not contain any results")
 else:
     output_full_path = os_path.join(tester_constants.RESULTS_FULL_PATH, output_file_name)
-    output_df.to_csv(output_full_path, index=False)
+    output_df.to_csv(output_full_path)
     print(f"Results merged to {output_full_path}")

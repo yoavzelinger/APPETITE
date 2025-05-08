7798425e1ae7d3b965abf4654532371b573cfec1
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-05-08 18:45:49 +0300

    continue of adding more similarities

--- APPETITE/Diagnosers/SFLDT.py
+++ APPETITE/Diagnosers/SFLDT.py
@@ -22,8 +22,9 @@ def get_faith_similarity(participation_vector: ndarray,
         error_vector (ndarray): The error vector, where high value (1) represent that the sample classified incorrectly.
 
     Returns:
-        float: The faith similarity between the vectors
+        float: The faith similarity between the vectors.
     """
+    print("using faith")
     n11 = participation_vector @ error_vector
     n10 = participation_vector @ (1 - error_vector)
     n01 = (1 - participation_vector) @ error_vector
@@ -94,21 +95,54 @@ def get_BCE_similarity(participation_vector: ndarray,
         is_binaries = is_participation_binary, is_error_binary
         print(is_binaries)
         assert not all(is_binaries), "Only one of the vectors should be binary"
+        assert any(is_binaries), "One of the vectors should be binary"
+        if is_participation_binary:
+            return participation_vector, error_vector
+        return error_vector, participation_vector
+    
+    binary_vector, continuous_vector = get_binary_continuous_vectors(participation_vector, error_vector)
+    continuous_vector = clip(continuous_vector, EPSILON, 1 - EPSILON)
+    bce_loss = -np_mean(binary_vector * np_log(continuous_vector) + (1 - binary_vector) * np_log(1 - continuous_vector))
+    return np_exp(-bce_loss)
+
+def get_relevant_similarity_function(example_participation_vector1: ndarray,
+                                     example_participation_vector2: ndarray,
+                                     error_vector: ndarray
+ ):
+    """
     Get the relevant similarity function based of the type of the vectors:
     if both are binary - use faith similarity.
+    if both are continuous - use correlation.
+    if one is binary and the other continuous - use BCE similarity.
+    Parameters:
+        example_participation_vector1 (ndarray): The first participation vector.
+        example_participation_vector2 (ndarray): The participation participation vector.
+            * Using two participation vectors that aren't on the same path to avoid wrong function due to all samples classified using the same path.
+        error_vector (ndarray): The error vector.
+    
+    Returns:
+        The relevant similarity function
+    """
+    is_participation1_binary = np_unique(example_participation_vector1).size <= 2
+    is_participation2_binary = np_unique(example_participation_vector2).size <= 2
+    is_participation_binary = all((is_participation1_binary, is_participation2_binary))
+    is_error_binary = np_unique(error_vector).size <= 2
+    is_binaries = is_participation_binary, is_error_binary
+    if all(is_binaries): # both binary
+        return get_faith_similarity
+    if any(is_binaries): # one binary one continuous
+        return get_BCE_similarity
+    # both continuous
+    return get_correlation
+
 class SFLDT(ADiagnoser):
 
     diagnoser_type = constants.SINGLE_DIAGNOSER_TYPE_NAME
 
-    similarity_measure_functions_dict = {
-        "faith": get_faith_similarity
-    }
-
     def __init__(self, 
                  mapped_tree: MappedDecisionTree,
                  X: DataFrame,
-                 y: Series,
-                 similarity_measure: str = constants.SFLDT_DEFAULT_SIMILARITY_MEASURES
+                 y: Series
     ):
         """
         Initialize the SFLDT diagnoser.
@@ -124,7 +158,6 @@ class SFLDT(ADiagnoser):
         self.tests_count = len(X)
         self.spectra = zeros((self.components_count, self.tests_count))
         self.error_vector = zeros(self.tests_count)
-        self.similarity_measure = similarity_measure
         self.fill_spectra_and_error_vector(X, y)
 
     def update_fuzzy_participation(self,
@@ -135,7 +168,7 @@ class SFLDT(ADiagnoser):
             return
         self.components_depths_vector = self.components_depths_vector[:, None]
         self.spectra = (self.spectra * self.components_depths_vector) / self.paths_depths_vector
-        assert max(self.spectra) <= 1.0, f"Participation should be in [0, 1] but got {max(self.spectra)}"
+        assert np_max(self.spectra) <= 1.0, f"Participation should be in [0, 1] but got {np_max(self.spectra)}"
 
     def fill_spectra_and_error_vector(self, 
                                       X: DataFrame, 
@@ -195,7 +228,9 @@ class SFLDT(ADiagnoser):
           where the first element is the index and the second is the similarity rank.
         """
         if self.diagnoses is None:
-            similarity_measure_function = SFLDT.similarity_measure_functions_dict[self.similarity_measure]
+            example_participation_vector1 = self.spectra[1] # Not using the 0 (root) because participated in all the tests
+            example_participation_vector2 = self.spectra[2] # Not using the 0 (root) because participated in all the tests
+            similarity_measure_function = get_relevant_similarity_function(example_participation_vector1, example_participation_vector2, self.error_vector)
             is_internal_node = lambda spectra_index: not self.mapped_tree.get_node(index=spectra_index, use_spectra_index=True).is_terminal()
             self.diagnoses = [(spectra_index, similarity_measure_function(self.spectra[spectra_index], self.error_vector) if is_internal_node(spectra_index) else 0) for spectra_index in range(self.components_count)]
             self.sort_diagnoses()

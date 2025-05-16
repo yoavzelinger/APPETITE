0867cf87b8514b8f2d4ff544e191d534647ad744
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-05-16 14:59:47 +0300

    avoid duplicated conversion of the diagnoses indices

--- APPETITE/Diagnosers/SFLDT.py
+++ APPETITE/Diagnosers/SFLDT.py
@@ -256,5 +256,5 @@ class SFLDT(ADiagnoser):
         if self.diagnoses is None:
             similarity_measure_function = self.get_relevant_similarity_function()
             self.diagnoses = [([spectra_index], similarity_measure_function(self.spectra[spectra_index], self.error_vector)) for spectra_index in range(self.components_count)]
-        self.convert_diagnoses_indices(retrieve_spectra_indices)
+            self.convert_diagnoses_indices(retrieve_spectra_indices)
         return super().get_diagnoses(retrieve_ranks)

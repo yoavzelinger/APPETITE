434460fe206949d537597022fcb05ed1df8508cf
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-07-21 16:11:26 +0300

    revert change in candidations to use the candidate spectra

--- APPETITE/Diagnosers/BARINEL.py
+++ APPETITE/Diagnosers/BARINEL.py
@@ -32,10 +32,12 @@ def get_barinel_diagnoses(spectra: np.ndarray,
     
     spectra = np.concatenate((spectra.T, error_vector[:, None]), axis=1)
     
-    diagnoses = get_candidates(spectra.tolist(), error_threshold=error_threshold)
-    if not diagnoses and candidates_spectra is not None:
+    if candidates_spectra is None:
+        candidates_spectra = spectra
+    else:
         candidates_spectra = np.concatenate((candidates_spectra.T, error_vector[:, None]), axis=1)
-        diagnoses = get_candidates(candidates_spectra.tolist(), error_threshold=error_threshold)
+    
+    diagnoses = get_candidates(candidates_spectra.tolist(), error_threshold=error_threshold)
     assert diagnoses, "No candidate diagnoses found"
     
     diagnoses = list(map(np.array, diagnoses))

cd1a1c9bfcb0848fd4e87a97503d17599deaea84
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-07-26 20:21:19 +0300

    add default parameter to whether include the fuzzy participation in the optimization

--- APPETITE/Constants.py
+++ APPETITE/Constants.py
@@ -9,12 +9,14 @@ BARINEL_COMPONENT_PRIOR_PROBABILITY = 1 / 1000
 # BARINEL ranking v1
 BARINEL_GRADIENT_STEP = 0.5
 
+# BARINEL ranking v2
+INCLUDE_FUZZY_PARTICIPATION_WHEN_OPTIMIZING = False
+
 # Choose the BARINEL ranking algorithm
 # V1: discrete error ranking algorithm taken from DDIFMAS
 # V2: new ranking algorithm, supporting fuzzy error and participation matrix, and custom prior probabilities
 BARINEL_RANKING_ALGORITHM_VERSION = 2
 
-GRADIENT_STEP = 0.5
 
 DEFAULT_COMBINE_STAT = True # Combine STAT Diagnoses
 DEFAULT_USE_SHAP_CONTRIBUTION = False # Try to use SHAP contributions for the participation

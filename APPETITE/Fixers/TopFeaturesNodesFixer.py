34bc9af2fe15fc929ddfa55288674e8b2c852db3
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-10-07 15:34:58 +0300

    add top nodes fixers

---
+++ APPETITE/Fixers/TopFeaturesNodesFixer.py
@@ -0,0 +1,24 @@
+from APPETITE.MappedDecisionTree import MappedDecisionTree
+
+from .AIndependentFixer import AIndependentFixer
+
+class TopFeaturesNodesFixer(AIndependentFixer):
+    def fix_tree(self
+     ) -> tuple[MappedDecisionTree, list[int]]:
+        """
+        Fix the decision tree.
+
+        Returns:
+            MappedDecisionTree: The fixed decision tree.
+            list[int]: The indices of the faulty nodes.
+        """
+        top_features_faults: dict[str, tuple[int, int]] = {}
+        for current_node in map(self.mapped_tree.get_node, self.faulty_nodes):
+            _, current_feature_fault_top_depth = top_features_faults.get(current_node.feature, (None, float('inf')))
+            if current_node.depth < current_feature_fault_top_depth:
+                top_features_faults[current_node.feature] = (current_node.index, current_node.depth)
+        for faulty_node_index, _ in top_features_faults.values():
+            data_reached_faulty_node = self._filter_data_reached_fault(faulty_node_index)
+            self.fix_faulty_node(faulty_node_index, data_reached_faulty_node)
+        self.tree_already_fixed = True
+        return super().fix_tree()
---
+++ APPETITE/Fixers/TopNodeFixer.py
@@ -0,0 +1,19 @@
+from APPETITE.MappedDecisionTree import MappedDecisionTree
+
+from .AIndependentFixer import AIndependentFixer
+
+class TopNodeFixer(AIndependentFixer):
+    def fix_tree(self
+     ) -> tuple[MappedDecisionTree, list[int]]:
+        """
+        Fix the decision tree.
+
+        Returns:
+            MappedDecisionTree: The fixed decision tree.
+            list[int]: The indices of the faulty nodes.
+        """
+        top_faulty_node_index = min(self.faulty_nodes, key=lambda node_index: self.mapped_tree.get_node(node_index).depth)
+        data_reached_top_faulty_node = self._filter_data_reached_fault(top_faulty_node_index)
+        self.fix_faulty_node(top_faulty_node_index, data_reached_top_faulty_node)
+        self.tree_already_fixed = True
+        return super().fix_tree()

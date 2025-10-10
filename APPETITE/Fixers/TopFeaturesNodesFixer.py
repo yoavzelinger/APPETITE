501df82abef1b41a52f7537a0dfbca7fcb6d99a6
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-10-10 14:05:54 +0300

    fix wrong member name

--- APPETITE/Fixers/TopFeaturesNodesFixer.py
+++ APPETITE/Fixers/TopFeaturesNodesFixer.py
@@ -17,7 +17,7 @@ class TopFeaturesNodesFixer(AIndependentFixer):
         for current_node in map(self.original_mapped_tree.get_node, self.faulty_nodes):
             _, current_feature_fault_top_depth = top_features_faults.get(current_node.feature, (None, float('inf')))
             if current_node.depth < current_feature_fault_top_depth:
-                top_features_faults[current_node.feature] = (current_node.index, current_node.depth)
+                top_features_faults[current_node.feature] = (current_node.sk_index, current_node.depth)
         for faulty_node_index, _ in top_features_faults.values():
             X_reached_faulty_node, _ = self._filter_data_reached_fault(faulty_node_index)
             self.fix_faulty_node(faulty_node_index, X_reached_faulty_node)

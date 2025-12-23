1626bc4aa425fb2d63f4cd762a000015fe5a27f2
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-12-23 17:14:26 +0200

    send the prior data to the fixer

--- Tester/single_test.py
+++ Tester/single_test.py
@@ -309,7 +309,7 @@ def run_single_test_v2(directory, file_name, file_extension: str = ".csv", repai
                     fixer_output_name = fixer_data.get("output_name", fixer_class_name)
                     fixer_parameters = fixer_data.get("parameters")
                     fixer_class = get_fixer(fixer_class_name)
-                    fixer: AFixer = fixer_class(mapped_tree, X_repair, y_repair, faulty_nodes=faulty_nodes_indices, **fixer_parameters)
+                    fixer: AFixer = fixer_class(mapped_tree, X_repair, y_repair, faulty_nodes=faulty_nodes_indices, X_prior=X_repair, y_prior=y_repair, **fixer_parameters)
                     fixed_tree = fixer.fix_tree()
                     fixed_test_accuracy = get_accuracy(fixed_tree, X_test, y_test)
                     test_accuracy_bump = fixed_test_accuracy - post_drift_test_accuracy
